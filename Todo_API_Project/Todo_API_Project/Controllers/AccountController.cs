961cf6bdc48469b1a170094a4dc37146cb134322
KatherineHanson <katherinemariehanson@gmail.com>
2018-05-14 12:48:13 -0700

    functional oauth

---
+++ Todo_API_Project/Todo_API_Project/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Todo_API_Project.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login(string returnUrl = "http://localhost:5000/signin-github")
+        {
+            return Challenge(new AuthenticationProperties() { RedirectUri = returnUrl });
+        }
+
+    }
+}
